d3edf6cdc898ee59d12e765c94c336e043600348
Song-Gi-Sung <rmltjddl123@naver.com>
2021-05-11 15:13:52 +0900

    git commit

--- src/main/java/com/practice/book/springboot/Application.java
+++ src/main/java/com/practice/book/springboot/Application.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정, 프로젝트의 최상단에 위치해야 함
 @SpringBootApplication
-@EnableJpaAuditing //JAP Auditing 활성화
+//@EnableJpaAuditing //JAP Auditing 활성화
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args); //.run으로 내장 WAS실행
--- src/main/java/com/practice/book/springboot/web/IndexController.java
+++ src/main/java/com/practice/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.practice.book.springboot.web;
 
+import com.practice.book.springboot.config.auth.LoginUser;
+import com.practice.book.springboot.config.auth.dto.SessionUser;
 import com.practice.book.springboot.service.posts.PostsService;
 import com.practice.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +10,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        //CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
+        if(user != null) { //세션에 저장된 값이 있을 때만 model에 userName으로 등록
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
